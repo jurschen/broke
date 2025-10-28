ad67d43701ec97b8483bac2239e7d4af06865508
jurschen <114289283+jurschen@users.noreply.github.com>
2025-10-28 13:30:58 +0100

    Update iOS platform version to v17

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Broke",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(name: "Broke", targets: ["Broke"])
     ],
