62428c98d0a37fd2de16f16a6a24d8d5b35b71b6
jurschen <114289283+jurschen@users.noreply.github.com>
2025-10-28 13:49:38 +0100

    Update Package.swift for dependency and platform

--- Package.swift
+++ Package.swift
@@ -1,28 +1,25 @@
-// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "Broke",
-    platforms: [
-        .iOS(.v17) // passt zu SFSymbolsPicker
-    ],
+    platforms: [.iOS(.v17)], // passt zu SFSymbolsPicker
     products: [
-        .library(
-            name: "Broke",
-            targets: ["Broke"]
-        ),
+        .library(name: "Broke", targets: ["Broke"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Nosajool/SFSymbolsPicker", from: "2.2.0")
+        // WICHTIG: richtige URL + Version
+        .package(url: "https://github.com/alessiorubicini/SFSymbolsPickerForSwiftUI", from: "1.0.8"),
     ],
     targets: [
         .target(
             name: "Broke",
-            dependencies: ["SFSymbolsPicker"],
+            // HIER explizit das Product referenzieren:
+            dependencies: [
+                .product(name: "SFSymbolsPicker", package: "SFSymbolsPicker")
+            ],
             path: "Broke",
-            // WICHTIG: App-/Demo-Dateien ausschließen:
             exclude: [
-                "BrokeApp.swift",        // enthält @main
+                "BrokeApp.swift",        // @main entfernen
                 "Info.plist",
                 "Assets.xcassets",
                 "Preview Content",
